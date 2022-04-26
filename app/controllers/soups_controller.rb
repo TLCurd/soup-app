a78321cfb8679dc16823e514e7a9e8bc09c68615
TLCurd <TLCurdChiro@gmail.com>
2022-04-26 12:20:57 -0600

    add index.html.erb; show.html.erb; working images link to show page

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,2 +1,12 @@
 class SoupsController < ApplicationController
+  def index
+    @soups = Soup.all
+    render "soups/index"
+  end
+
+  def show
+    @soup = Soup.find_by(id: params[:id])
+    render "soups/show"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ get '/soups', to: 'soups#index'
+ get '/soups/:id', to: 'soups#show'
 end
